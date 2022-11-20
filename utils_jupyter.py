a68874e53f0a961ac0bbf06f1addab2f154a89e1
maximofn <maximofn@gmail.com>
2022-11-20 01:21:21 +0100

    Añado lineas para depurar

--- utils_jupyter.py
+++ utils_jupyter.py
@@ -1,5 +1,7 @@
 import json
 
+DEBUG = True
+
 def get_notebook_as_dict(filename):
     with open(filename, 'r') as f:
         notebook = json.load(f)
@@ -18,32 +20,78 @@ def get_headers(cells):
             headers.append(cell['source'][0])
     return headers
 
+def write_end_of_line(file, i, N):
+    if i == N-1:
+        string = "\n"
+        file.write(string)
+        if DEBUG: print(string, end='')
+    else:
+        string = ",\n"
+        file.write(string)
+        if DEBUG: print(string, end='')
+
 def write_dict_to_file(file, dictionary, indentation):
-    file.write("{\n")
+    string = "{\n"
+    file.write(string)
+    if DEBUG: print(string, end='')
     indentation += 1
     for i, key in enumerate(dictionary.keys()):
-        file.write(((" ")*2*indentation)+f"\"{key}\": \"{dictionary[key]}\"")
+        string = ((" ")*2*indentation)+f"\"{key}\": "
+        file.write(string)
+        if DEBUG: print(string, end='')
         if type(dictionary[key]) == dict:
             write_dict_to_file(file, dictionary[key], indentation)
         elif type(dictionary[key]) == list:
-            pass
-            # write_list_to_file(file, dictionary[key], indentation)
-            # file.write(((" ")*2*indentation)+"[")
-            # for item in dictionary[key]:
-            #     file.write("\""+item+"\",")
-            # file.write("]")
+            write_list_to_file(file, dictionary[key], indentation)
         elif type(dictionary[key]) == str:
-            pass
-            # write_str_to_file(file, dictionary[key], indentation)
+            write_str_to_file(file, dictionary[key], indentation)
         else:
-            pass
-            # write_rest_to_file(file, dictionary[key], indentation)
-        if i == len(dictionary.keys())-1:
-            file.write("\n")
-        else:
-            file.write(",\n")
+            write_rest_to_file(file, dictionary[key], indentation)
+        write_end_of_line(file, i, len(dictionary.keys()))
     indentation -= 1
-    file.write(((" ")*2*indentation)+"}")    
+    string = ((" ")*2*indentation)+"}"
+    file.write(string)
+    if DEBUG: print(string, end='')
+    return indentation
+
+def write_list_to_file(file, list, indentation):
+    if len(list) > 0:
+        string = "[\n"+((" ")*2*indentation)
+        file.write(string)
+        if DEBUG: print(string, end='')
+        indentation += 1
+        for i, item in enumerate(list):
+            if i > 0:
+                break
+            if type(item) == dict:
+                write_dict_to_file(file, item, indentation)
+            elif type(item) == list:
+                write_list_to_file(file, item, indentation)
+            elif type(item) == str:
+                write_str_to_file(file, item, indentation)
+            else:
+                write_rest_to_file(file, item, indentation)
+            write_end_of_line(file, i, len(list))
+        indentation -= 1
+        string = ((" ")*2*indentation)+"]"
+        file.write(string)
+        if DEBUG: print(string, end='')
+    else:
+        string = "[]"
+        file.write(string)
+        if DEBUG: print(string, end='')
+    return indentation
+
+def write_str_to_file(file, string, indentation):
+    string = f"\"{string}\""
+    file.write(string)
+    if DEBUG: print(string, end='')
+    return indentation
+
+def write_rest_to_file(file, rest, indentation):
+    string = f"\"{rest}\""
+    file.write(string)
+    if DEBUG: print(string, end='')
     return indentation
 
 def dict_to_ipynb(notebook, filename):
@@ -56,137 +104,24 @@ def dict_to_ipynb(notebook, filename):
     indentation += 1
 
     notebook_keys = list(notebook.keys())
+    print(type(notebook_keys))
     for k, key in enumerate(notebook_keys):
         if type(notebook[key]) == dict:
-            print(f"Key {key} is a dict")
-            new_notebook.write(((" ")*2*indentation)+"\""+key+"\": {\n")
-            indentation += 1
-            dict_keys = list(notebook[key].keys())
-            for dk, dict_key in enumerate(dict_keys):
-                new_notebook.write(((" ")*2*indentation)+"\""+dict_key+"\": {\n")
-                indentation += 1
-                if type(notebook[key][dict_key]) == str:
-                    new_notebook.write(((" ")*2*indentation)+"\""+notebook[key][dict_key]+"\"")
-                elif type(notebook[key][dict_key]) == list:
-                    new_notebook.write(((" ")*2*indentation)+"[")
-                    for item in notebook[key][dict_key]:
-                        new_notebook.write("\""+item+"\",")
-                    new_notebook.write("]")
-                elif type(notebook[key][dict_key]) == dict:
-                    for sk, subkey in enumerate(notebook[key][dict_key].keys()):
-                        new_notebook.write(((" ")*2*indentation)+"\""+subkey+"\": ")
-                        if type(notebook[key][dict_key][subkey]) == str:
-                            new_notebook.write("\""+notebook[key][dict_key][subkey]+"\"")
-                        elif type(notebook[key][dict_key][subkey]) == list:
-                            new_notebook.write("[")
-                            for item in notebook[key][dict_key][subkey]:
-                                new_notebook.write(((" ")*2*indentation)+"\""+item+"\",")
-                            new_notebook.write(((" ")*2*indentation)+"]")
-                        elif type(notebook[key][dict_key][subkey]) == dict:
-                            print(f"Key {subkey} is a dict")
-                            indentation = write_dict_to_file(new_notebook, notebook[key][dict_key][subkey], indentation)
-                            # new_notebook.write("{\n")
-                            # indentation += 1
-                            # for ssk, subsubkey in enumerate(notebook[key][dict_key][subkey].keys()):
-                            #     new_notebook.write(((" ")*2*indentation)+"\""+subsubkey+"\": \""+str(notebook[key][dict_key][subkey][subsubkey])+"\"")
-                            #     if ssk == len(notebook[key][dict_key][subkey].keys())-1:
-                            #         new_notebook.write("\n")
-                            #     else:
-                            #         new_notebook.write(",\n")
-                            # indentation -= 1
-                            # new_notebook.write(((" ")*2*indentation)+"}")
-                        else:
-                            new_notebook.write(str(notebook[key][dict_key][subkey]).lower())
-                        if sk == len(notebook[key][dict_key].keys())-1:
-                            new_notebook.write("\n")
-                        else:
-                            new_notebook.write(",\n")
-                else:
-                    new_notebook.write(((" ")*2*indentation)+str(notebook[key][dict_key])+"")
-                indentation -= 1
-                if dk == len(dict_keys)-1:
-                    new_notebook.write(((" ")*2*indentation)+"}\n")
-                else:
-                    new_notebook.write(((" ")*2*indentation)+"},\n")
-                # new_notebook.write(((" ")*2*indentation)+"},\n")
-            indentation -= 1
-            if k == len(notebook_keys)-1:
-                new_notebook.write(((" ")*2*indentation)+"}\n")
-            else:
-                new_notebook.write(((" ")*2*indentation)+"},\n")
+            new_notebook.write(((" ")*2*indentation)+"\""+key+"\": ")
+            indentation = write_dict_to_file(new_notebook, notebook[key], indentation)
+            write_end_of_line(new_notebook, k, len(notebook_keys))
         elif type(notebook[key]) == list:
-            print(f"Key {key} is a list")
-            pass
-            # new_notebook.write(((" ")*2*indentation)+"\""+key+"\": [\n")
-            # indentation += 1
-            # for i, item in enumerate(notebook[key]):
-            #     if i > 1:
-            #         break
-            #     new_notebook.write(((" ")*2*indentation)+"{\n")
-            #     indentation += 1
-            #     if type(item) == dict:
-            #         for ik, item_key in enumerate(item.keys()):
-            #             new_notebook.write(((" ")*2*indentation)+"\""+item_key+"\": ")
-            #             if type(item[item_key]) == str:
-            #                 new_notebook.write("\""+item[item_key]+"\"")
-            #             elif type(item[item_key]) == list:
-            #                 new_notebook.write("[")
-            #                 indentation += 1
-            #                 for ii, item_item in enumerate(item[item_key]):
-            #                     if type(item_item) == str:
-            #                         new_notebook.write(((" ")*2*indentation)+"\""+item_item+"\"")
-            #                     elif type(item_item) == dict:
-            #                         new_notebook.write(((" ")*2*indentation)+"{\n")
-            #                         indentation += 1
-            #                         for iii, item_item_item in enumerate(item_item.keys()):
-            #                             new_notebook.write(((" ")*2*indentation)+"\""+item_item_item+"\": \""+item_item[item_item_item]+"\"")
-            #                             if iii == len(item_item.keys())-1:
-            #                                 new_notebook.write("\n")
-            #                             else:
-            #                                 new_notebook.write(",\n")
-            #                         indentation -= 1
-            #                         new_notebook.write(((" ")*2*indentation)+"}")
-            #                     else:
-            #                         new_notebook.write(((" ")*2*indentation)+str(item_item))
-            #                     if ii == len(item[item_key])-1:
-            #                         new_notebook.write("\n")
-            #                     else:
-            #                         new_notebook.write(",\n")
-            #                 indentation -= 1
-            #                 new_notebook.write("]")
-            #             elif type(item[item_key]) == dict:
-            #                 new_notebook.write("{\n")
-            #                 indentation += 1
-            #                 for iik, item_item_key in enumerate(item[item_key].keys()):
-            #                     new_notebook.write(((" ")*2*indentation)+"\""+item_item_key+"\": \""+str(item[item_key][item_item_key])+"\"")
-            #                     if iik == len(item[item_key].keys())-1:
-            #                         new_notebook.write("\n")
-            #                     else:
-            #                         new_notebook.write(",\n")
-            #                 indentation -= 1
-            #                 new_notebook.write(((" ")*2*indentation)+"}")
-            #             else:
-            #                 new_notebook.write(str(item[item_key]).lower())
-            #             if ik == len(item.keys())-1:
-            #                 new_notebook.write("\n")
-            #             else:
-            #                 new_notebook.write(",\n")
-            #     indentation -= 1
-            #     if i == len(notebook[key])-1:
-            #         new_notebook.write(((" ")*2*indentation)+"}\n")
-            #     else:
-            #         new_notebook.write(((" ")*2*indentation)+"},\n")
-            # indentation -= 1
-            # if k == len(notebook_keys)-1:
-            #     new_notebook.write(((" ")*2*indentation)+"]\n")
-            # else:
-            #     new_notebook.write(((" ")*2*indentation)+"],\n")
+            new_notebook.write(((" ")*2*indentation)+"\""+key+"\": ")
+            indentation = write_list_to_file(new_notebook, notebook[key], indentation)
+            write_end_of_line(new_notebook, k, len(notebook_keys))
+        elif type(notebook[key] == str):
+            new_notebook.write(((" ")*2*indentation)+"\""+key+"\": ")
+            indentation = write_str_to_file(new_notebook, notebook[key], indentation)
+            write_end_of_line(new_notebook, k, len(notebook_keys))
         else:
-            new_notebook.write(((" ")*2*indentation)+"\""+key+"\": "+str(notebook[key]))
-            if k == len(notebook_keys)-1:
-                new_notebook.write("\n")
-            else:
-                new_notebook.write(",\n")
+            new_notebook.write(((" ")*2*indentation)+"\""+key+"\": ")
+            indentation = write_rest_to_file(new_notebook, notebook[key], indentation)
+            write_end_of_line(new_notebook, k, len(notebook_keys))
     indentation -= 1
     new_notebook.write(((" ")*2*indentation)+"}\n")
 
