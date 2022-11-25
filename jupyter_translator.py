d3043abe37748fe6110cf6360d2d42f6fa63454c
maximofn <maximofn@gmail.com>
2022-11-25 08:51:15 +0100

    Se arregla el que se puedan meter varios idiomas por argumento. Se arregla que cuando hay varios idiomas, la traducción de uno no pise la traducción de otro

--- jupyter_translator.py
+++ jupyter_translator.py
@@ -20,10 +20,11 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Jupyter notebook translator')
     parser.add_argument('-f', '--file', help='The notebook to translate', required=True)
     # parser.add_argument('-t', '--target', help='The target language, can be a list of languajes', default=['EN'])
-    parser.add_argument('-t', '--target', help='The target language, can be a list of languajes', default=['PT-BR'])
-    # parser.add_argument('-t', '--target', help='The target language, can be a list of languajes', required=True)
+    # parser.add_argument('-t', '--target', help='The target language, can be a list of languajes', default=['PT-BR'])
+    parser.add_argument('-t', '--target', nargs='+', help='The target language, can be a list of languajes', required=True)
     
     args = parser.parse_args()
+    print(args.target, type(args.target))
     if args.file:
         _, name, extension, _ = path_name_ext_from_file(args.file)
         if extension == '.ipynb':
@@ -61,7 +62,7 @@ def main():
     for lang in args.target:
         if lang in target_lang.keys():
             lang = target_lang[lang]
-        notebooks_translated.append(notebook)
+        notebooks_translated.append(uj.get_notebook_as_dict(args.file))
 
     # Get cells of the notebook
     print("Getting cells of the notebook")
@@ -76,7 +77,7 @@ def main():
     bar = tqdm(cells, bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}')
     total_cells = len(cells)
     for c, cell in enumerate(bar):
-        # if c == 2:
+        # if c == 4:
         #     break;
         if cell['cell_type'] == 'markdown':
             for l, lang in enumerate(args.target):
@@ -89,7 +90,26 @@ def main():
                         notebooks_translated[l]['cells'][c]['source'][j] = ud.translate_text(line, lang, translator)
         bar.set_description(f"\tCell {c}/{total_cells}")
     print(f"End of translation")
-    
+
+    # Add autimatic translation warning message
+    for l, lang in enumerate(args.target):
+        if lang in target_lang.keys():
+            lang = target_lang[lang]
+        if lang == 'EN' or lang == 'EN-GB' or lang == 'EN-US':
+            warning_string = "This notebook has been automatically translated to make it accessible to more people, please let me know if you see any typos.\n"
+            notebooks_translated[0]['cells'][2]['source'].insert(0, warning_string)
+            notebooks_translated[0]['cells'][2]['source'].insert(1, "\n")
+        elif lang == 'PT' or lang == 'PT-BR' or lang == 'PT-PT':
+            warning_string = "Este caderno foi traduzido automaticamente para torná-lo acessível a mais pessoas, por favor me avise se você vir algum erro de digitação..\n"
+            notebooks_translated[1]['cells'][2]['source'].insert(0, warning_string)
+            notebooks_translated[1]['cells'][2]['source'].insert(1, "\n")
+        else:
+            warning_string = "Este notebook ha sido traducido automáticamente para que sea accesible por más gente, por favor, si ves alguna errata házmelo saber"
+            warning_string = ud.translate_text(warning_string, lang, translator)
+            warning_string = warning_string + "\n"
+            notebooks_translated[0]['cells'][2]['source'].insert(0, warning_string)
+            notebooks_translated[0]['cells'][2]['source'].insert(1, "\n")
+
     # Save the translated notebooks
     for l, lang in enumerate(args.target):
         if lang in target_lang.keys():
